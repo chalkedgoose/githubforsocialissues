f4e99da88431a332592414b89638d04565d28246
Edwin Cloud <ecloud412@gmail.com>
2018-10-29 13:18:16 -0700

    :tada: Issues route and controller finished

--- routes.py
+++ routes.py
@@ -4,6 +4,8 @@
 from server import api
 from controllers.api import ApiRoutes
 from controllers.cities import CityRoutes
+from controllers.issues import IssueRoutes
 
 api.add_route('/', ApiRoutes())
 api.add_route('/cities', CityRoutes())
+api.add_route('/issues', IssueRoutes())
