65d9626eae0396b8d457b6366a73614941b9e825
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-10-30 15:51:26 -0700

    api tracker skeleton

---
+++ apiTrackerGo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"context"
+	"log"
+	"github.com/mongodb/mongo-go-driver/mongo"
+)
+
+// MONGOURI: mongodb://localhost:27017/githubforsocialissues
+
+func main() {
+
+	client, err := mongo.NewClient("mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	// The Reqs connection from the MongoDatabase
+	collection := client.Database("githubforsocialissues").Collection("reqs")
+
+
+	fmt.Println(collection)
+
+
+}
