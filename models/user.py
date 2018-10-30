b4867b693080d6cef8d28be8295f4407f8dc4e43
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-10-30 11:14:44 -0700

    user

--- models/user.py
+++ models/user.py
@@ -2,16 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from mongoengine import *
+from models.city import Cities as City
 
 
 class Users(Document):
+    ''' creates user  document '''
     avatar = StringField(required=True, default='')
     first_name = StringField(required=True)
     last_name = StringField(required=True)
     email = StringField(required=True)
     password = StringField(required=True)
+    city = ReferenceField(City)
 
     def to_json(query_object):
+        ''' converts User to json '''
         return {
             "id": str(query_object.id),
             "avatar": query_object.avatar,
