543bf6cf332e2c115c680a01999f92425dbf1af7
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-10-30 11:08:38 -0700

    issues model city field

--- models/city.py
+++ models/city.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from mongoengine import *
+# from mongoengine import *
+from mongoengine import Document
 
 
 class Cities(Document):
+    ''' City Class with Mongo Features '''
     name = StringField(required=True)
     state = StringField(required=True)
     country = StringField(required=True)
 
     def to_json(query_object):
-        return {
-            "id": str(query_object.id),
-            "name": query_object.name,
-            "state": query_object.state,
-            "country": query_object.country
-        }
+    ''' returns object as dict '''
+    return {
+        "id": str(query_object.id),
+        "name": query_object.name,
+        "state": query_object.state,
+        "country": query_object.country
+    }
--- models/issue.py
+++ models/issue.py
@@ -3,6 +3,7 @@
 
 from mongoengine import *
 from models.user import Users as User
+from models.city import Cities as City
 
 
 class Issues(Document):
@@ -10,12 +11,15 @@ class Issues(Document):
     description = StringField(required=True)
     author = ReferenceField(User)
     resolved_by = ReferenceField(User)
+    city = ReferenceField(City)
 
     def to_json(query_object):
+        ''' returns Issue as a dict '''
         return {
             "id": str(query_object.id),
             "title": query_object.title,
             "description": query_object.description,
             "author": str(query_object.author),
-            "resolved_by": str(query_object.resolved_by)
+            "resolved_by": str(query_object.resolved_by),
+            "city": str(query_object.city)
         }
