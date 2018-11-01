b869f9f3ff940f5ed8a91a64e0f19aa2d699398d
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-10-31 20:30:18 -0700

    notificationservice boiler

--- apitracker/controllers/MainRouter.js
+++ apitracker/controllers/MainRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const MainRouter = express.Router();
 const mongoose = require('mongoose')
-const circularJSON = require('circular-json');
 const Req = require('../models/req')
 
 
@@ -9,6 +8,10 @@ MainRouter.get('/requests', (req, res) => {
     /**
      * Gets all the requests back from the mongoDatabase and sends them to the client 
      */
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     Req.find().lean().then(reqs => {
         res.json(reqs)
     }).catch(err => {
