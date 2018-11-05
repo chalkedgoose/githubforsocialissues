024906d7bbe08e3257d859cc5210620613d34eb2
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-05 12:53:07 -0800

    working Nexmo SMS integration still working with SendGrid

--- notificationService/notification.py
+++ notificationService/notification.py
@@ -1,6 +1,7 @@
 import sendgrid
 import os
 from sendgrid.helpers.mail import *
+import nexmo
 
 
 class Notification(object):
@@ -9,6 +10,8 @@ class Notification(object):
         self.sg = sendgrid.SendGridAPIClient(
             apikey=os.environ.get('SENDGRID_API_KEY'))
         self.from_email = "atom@projectpescadero.com"
+        self.client = nexmo.Client(key='bcefb8f8', secret='G4BbrKWlbWhTF1dI')
+        self.from_number = '13364772034'
 
     def sendEmail(self, to_email, subject, content):
         mail = Mail(self.from_email, subject, to_email, content)
@@ -17,11 +20,26 @@ class Notification(object):
         print(response.body)
         print(response.headers)
 
+    def sendSMS(self, to_number, text):
+
+        self.client.send_message(
+            {
+                'from': self.from_number,
+                'to': to_number,
+                'text': text
+            }
+        )
+
 
 if __name__ == "__main__":
 
     xNotifier = Notification()
-    email = "carlos.alba@students.makeschool.com"
-    subject = "Sendgrid Test"
-    content = "<h1> This is a message sent from the notifiaction service on Project Pescadero </h1>"
-    xNotifier.sendEmail(email, subject, content)
+    # sms
+    xNotifier.sendSMS('14159008591', 'Nexmo test')
+
+    # email not working
+
+    # email = "carlos.alba@students.makeschool.com"
+    # subject = "Sendgrid Test"
+    # content = "<h1> This is a message sent from the notifiaction service on Project Pescadero </h1>"
+    # xNotifier.sendEmail(email, subject, content)
